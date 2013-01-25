f53fb52b88e59635a4f6d7a3966085310b865270
Vadim Platonov <vadim@inventi.lt>
2013-01-25 16:31:06 +0200

    Default bootstrap control group border with label

---
+++ inventi-wicket-bootstrap/src/main/java/lt/inventi/wicket/component/bootstrap/form/ControlGroup.java
@@ -0,0 +1,57 @@
+package lt.inventi.wicket.component.bootstrap.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.border.Border;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.util.iterator.ComponentHierarchyIterator;
+
+public class ControlGroup extends Border {
+
+    private final Label label = new Label("label");
+
+    public ControlGroup(String id) {
+        super(id);
+
+        addToBorder(label);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        setRenderBodyOnly(true);
+        String resourcePrefix = "";
+
+        Form<?> form = findParentForm();
+        if (form != null) {
+            resourcePrefix = form.getId() + ".";
+        }
+
+        List<FormComponent<?>> formComponents = findFormComponents();
+        label.setDefaultModel(new ResourceModel(resourcePrefix + formComponents.get(0).getId()));
+    }
+
+    private Form<?> findParentForm() {
+        MarkupContainer parent = getParent();
+        while (parent != null && !(parent instanceof Form<?>)) {
+            parent = parent.getParent();
+        }
+        return (Form<?>) parent;
+    }
+
+    private List<FormComponent<?>> findFormComponents() {
+        ComponentHierarchyIterator it = getBodyContainer().visitChildren(FormComponent.class);
+
+        List<FormComponent<?>> components = new ArrayList<FormComponent<?>>();
+        while (it.hasNext()) {
+            components.add((FormComponent<?>) it.next());
+        }
+
+        return components;
+    }
+}
