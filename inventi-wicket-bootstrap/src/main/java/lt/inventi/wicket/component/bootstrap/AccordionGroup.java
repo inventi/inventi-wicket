7231cd35eaacc47a1d551f1d10af2919a3c41515
Vadim Platonov <vadim@inventi.lt>
2013-01-28 15:59:39 +0200

    Add options to accordion for uncollapsed/no-parent state

--- inventi-wicket-bootstrap/src/main/java/lt/inventi/wicket/component/bootstrap/AccordionGroup.java
+++ inventi-wicket-bootstrap/src/main/java/lt/inventi/wicket/component/bootstrap/AccordionGroup.java
@@ -1,7 +1,10 @@
 package lt.inventi.wicket.component.bootstrap;
 
+import java.io.Serializable;
+
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -21,12 +24,45 @@ import lt.inventi.wicket.resource.ResourceSettings;
  */
 public class AccordionGroup extends Border {
 
+    public static class Options implements Serializable {
+        private boolean collapsed = true;
+        private boolean hasParent = true;
+
+        public static Options collapsed() {
+            return new Options().setCollapsed(true);
+        }
+
+        public static Options unCollapsed() {
+            return new Options().setCollapsed(false);
+        }
+
+        public Options withoutParent() {
+            return setHasParent(false);
+        }
+
+        public Options setHasParent(boolean hasParent) {
+            this.hasParent = hasParent;
+            return this;
+        }
+
+        public Options setCollapsed(boolean collapsed) {
+            this.collapsed = collapsed;
+            return this;
+        }
+    }
+
     private final Label label;
+    private final Options options;
 
     public AccordionGroup(String id, IModel<String> labelModel) {
+        this(id, labelModel, new Options());
+    }
+
+    public AccordionGroup(String id, IModel<String> labelModel, Options options) {
         super(id);
         setRenderBodyOnly(true);
         label = new Label("label", labelModel);
+        this.options = options;
     }
 
     @Override
@@ -36,7 +72,14 @@ public class AccordionGroup extends Border {
         WebMarkupContainer body = new WebMarkupContainer("body");
         body.setOutputMarkupId(true);
 
-        label.add(new AttributeModifier("data-parent", idModel(getParent())));
+        if (options.collapsed) {
+            body.add(new AttributeAppender("class", " collapse"));
+        } else {
+            body.add(new AttributeAppender("class", " in"));
+        }
+        if (options.hasParent) {
+            label.add(new AttributeModifier("data-parent", idModel(getParent())));
+        }
         label.add(new AttributeModifier("href", idModel(body)));
 
         addToBorder(label, body);
