53316390ce33d4e4fa6c46c2aae21d6b07f99aba
Vadim Platonov <vadim@inventi.lt>
2013-02-25 19:34:32 +0200

    Fix breadcrumbs tests

--- inventi-wicket-breadcrumbs/src/test/java/lt/inventi/wicket/component/breadcrumb/BreadcrumbsTests.java
+++ inventi-wicket-breadcrumbs/src/test/java/lt/inventi/wicket/component/breadcrumb/BreadcrumbsTests.java
@@ -14,9 +14,12 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 
+import lt.inventi.wicket.component.breadcrumb.collapse.DisplayedBreadcrumb;
+import lt.inventi.wicket.test.FuzzyWicketTester;
+
 public class BreadcrumbsTests {
 
-    protected WicketTester tester = new WicketTester();
+    protected WicketTester tester = new FuzzyWicketTester();
 
     @Before
     public void setUp() {
@@ -32,10 +35,10 @@ public class BreadcrumbsTests {
     @SuppressWarnings("unchecked")
     protected Iterable<? extends String> breadcrumbTitles() {
         Component c = tester.getComponentFromLastRenderedPage("crumbs:crumbs");
-        ListView<Breadcrumb> view = ((ListView<Breadcrumb>) c);
+        ListView<DisplayedBreadcrumb> view = ((ListView<DisplayedBreadcrumb>) c);
         List<String> titles = new ArrayList<String>();
-        for (Breadcrumb b : view.getList()) {
-            titles.add(b.getTitleModel().getObject());
+        for (DisplayedBreadcrumb b : view.getList()) {
+            titles.add(b.title().getObject());
         }
         return titles;
     }
