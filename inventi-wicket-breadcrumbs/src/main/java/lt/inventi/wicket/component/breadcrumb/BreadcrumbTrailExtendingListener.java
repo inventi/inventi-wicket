2aa0980bfaaff66f5f6efd26d774f0c650fb7fc6
Vadim Platonov <vadim@inventi.lt>
2013-02-26 10:50:13 +0200

    Replace system out with proper logging

--- inventi-wicket-breadcrumbs/src/main/java/lt/inventi/wicket/component/breadcrumb/BreadcrumbTrailExtendingListener.java
+++ inventi-wicket-breadcrumbs/src/main/java/lt/inventi/wicket/component/breadcrumb/BreadcrumbTrailExtendingListener.java
@@ -11,6 +11,8 @@ import org.apache.wicket.application.IComponentOnBeforeRenderListener;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.component.IRequestablePage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class BreadcrumbTrailExtendingListener implements IComponentOnBeforeRenderListener {
@@ -18,10 +20,11 @@ public class BreadcrumbTrailExtendingListener implements IComponentOnBeforeRende
      * We cannot use {@link IComponentInitializationListener} or
      * {@link IComponentInstantiationListener} because navigation to the
      * breadcrumbs in the trail won't always create/initialize pages (in case
-     * they're stateful and still in page store). We construct unique trail ids
-     * based on the page's hashCode and a deserialized page's hashCode doesn't
-     * have to match the hashCode of the serialized one, thus we cannot reliably
-     * find the previous page trail without extending it on each render.
+     * they're stateful and still in the page store). We construct unique trail
+     * ids based on the page's hashCode and a deserialized page's hashCode
+     * doesn't have to match the hashCode of the serialized one, thus we cannot
+     * reliably find the previous page trail without extending it on each
+     * render.
      * <p>
      * Possible improvement: store the newly created breadcrumb in the session's
      * metadata and use it in all further next page links. This way we can use
@@ -29,6 +32,7 @@ public class BreadcrumbTrailExtendingListener implements IComponentOnBeforeRende
      * breadcrumbs when the same page is rerendered (same page id + class but
      * different page object and thus hashCode).
      */
+    private static final Logger logger = LoggerFactory.getLogger(BreadcrumbTrailExtendingListener.class);
 
     private final IBreadcrumbPageFilter pageFilter;
 
@@ -38,12 +42,22 @@ public class BreadcrumbTrailExtendingListener implements IComponentOnBeforeRende
 
     @Override
     public void onBeforeRender(Component component) {
-        if (component instanceof Page && pageFilter.shouldCreateBreadcrumbFor((IRequestablePage) component)) {
-            Page p = (Page) component;
-            Breadcrumb crumb = createBreadcrumbFrom(p);
-            String maybeTrailId = getOptionalTrailId(p.getPageParameters());
-            BreadcrumbTrailHistory.extendTrail(maybeTrailId, crumb);
-            System.out.println("Session: " + Session.get().getSizeInBytes() + " b.");
+        if (component instanceof Page) {
+            if (pageFilter.shouldCreateBreadcrumbFor((IRequestablePage) component)) {
+                Page p = (Page) component;
+                Breadcrumb crumb = createBreadcrumbFrom(p);
+                String maybeTrailId = getOptionalTrailId(p.getPageParameters());
+                BreadcrumbTrailHistory.extendTrail(maybeTrailId, crumb);
+
+                if (logger.isTraceEnabled()) {
+                    logger.trace("Extended breadcrumb trail for trail {} with crumb {}", maybeTrailId, crumb);
+                    logger.trace("Session size after extending trail: " + Session.get().getSizeInBytes() + " bytes");
+                }
+            } else {
+                if (logger.isTraceEnabled()) {
+                    logger.trace("Skipping breadcrumb trail extension for page: " + component);
+                }
+            }
         }
     }
 
