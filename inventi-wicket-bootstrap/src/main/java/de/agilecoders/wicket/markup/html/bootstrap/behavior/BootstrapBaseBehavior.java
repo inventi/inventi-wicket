753b1565fd611a652c05d30548bceb5b01cfd9ed
Vadim Platonov <vadim@inventi.lt>
2013-03-15 19:30:29 +0200

    Update wicket to 6.6, bootstrap to 0.8

--- inventi-wicket-bootstrap/src/main/java/de/agilecoders/wicket/markup/html/bootstrap/behavior/BootstrapBaseBehavior.java
+++ inventi-wicket-bootstrap/src/main/java/de/agilecoders/wicket/markup/html/bootstrap/behavior/BootstrapBaseBehavior.java
@@ -1,7 +1,10 @@
 package de.agilecoders.wicket.markup.html.bootstrap.behavior;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
 
 public class BootstrapBaseBehavior extends Behavior {
-    // empty
+    public static void addTo(final Component component) {
+        // do nothing
+    }
 }
