75cbaddd415401645605734f4bc672e43b35900d
Vadim Platonov <vadim@inventi.lt>
2013-04-03 16:47:38 +0300

    Add conditional list with an empty container

---
+++ inventi-wicket-generic/src/main/java/lt/inventi/wicket/component/repeater/ConditionalListView.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2013 55 Minutes (http://www.55minutes.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package lt.inventi.wicket.component.repeater;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
+
+/**
+ * A {@link ListView} that returns <code>false</code> for {@link #isVisible} if
+ * its list is empty. This allows you to use the
+ * <code>&lt;wicket:enclosure&gt;</code> tag to hide surrounding markup.
+ * Therefore you can use this component to ensure that, for example, the
+ * <code>&lt;ul&gt;</code> is ommitted when you have zero
+ * <code>&lt;li&gt;</code> elements.
+ * <em>This is necessary for valid XHTML markup.</em>
+ * <p>
+ * Example usage:
+ *
+ * <pre class="example">
+ * &lt;wicket:enclosure child="item"&gt;
+ *   &lt;ul&gt;
+ *     &lt;li wicket:id=&quot;item&quot;&gt;...&lt;/li&gt;
+ *   &lt;/ul&gt;
+ * &lt;/wicket:enclosure&gt;
+ *
+ * add(new ConditionalListView&lt;Person&gt;("people", personList) {
+ *     &#064;Override protected void populateItem(ListItem&lt;Person&gt; item)
+ *     {
+ *         item.add(...);
+ *     }
+ * });
+ * </pre>
+ * <p>
+ * In this example, if the {@code personList} model is {@code null} or empty,
+ * the entire enclosure will be omitted from the HTML output.
+ */
+public abstract class ConditionalListView<T> extends ListView<T>
+{
+    public ConditionalListView(String id) {
+        super(id);
+    }
+
+    public ConditionalListView(String id, IModel<? extends List<? extends T>> model) {
+        super(id, model);
+    }
+
+    public ConditionalListView(String id, List<? extends T> list) {
+        super(id, list);
+    }
+
+    /**
+     * Returns <code>false</code> if the list is empty.
+     */
+    @Override
+    public boolean isVisible() {
+        return getViewSize() > 0;
+    }
+}
---
+++ inventi-wicket-generic/src/main/java/lt/inventi/wicket/component/repeater/EmptyContainer.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2013 55 Minutes (http://www.55minutes.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package lt.inventi.wicket.component.repeater;
+
+import java.util.Collection;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.string.Strings;
+
+/**
+ * A WebMarkupContainer that is visible only if its model object is empty. This
+ * is useful for displaying a conditional message when an accompanying list view
+ * is empty.
+ * <p>
+ * For example:
+ *
+ * <pre class="example">
+ * &lt;wicket:enclosure&gt;
+ *   &lt;ul&gt;
+ *     &lt;li wicket:id="friends"&gt;...&lt;/li&gt;
+ *   &lt;/ul&gt;
+ * &lt;/wicket:enclosure&gt;
+ * &lt;p wicket:id="no-friends"&gt;
+ *   Your friends list is empty.
+ * &lt;/p&gt;
+ * </pre>
+ *
+ * <pre class="example">
+ * IModel&lt;List&lt;Friend&gt;&gt; friendsModel = ...;
+ * add(new ConditionalListView("friends", friendsModel) {
+ *     &#064;Override
+ *     protected void populateItem(ListItem&lt;Friend&gt; item)
+ *     {
+ *         ...
+ *     }
+ * });
+ * // This will be visible only when friendsModel is empty
+ * add(new EmptyContainer("no-friends", friendsModel));
+ * </pre>
+ *
+ * @since 2.0
+ */
+public class EmptyContainer extends WebMarkupContainer {
+    /**
+     * Constructor with an implied model.
+     */
+    public EmptyContainer(String id) {
+        super(id);
+    }
+
+    /**
+     * Constructor with an explicit model.
+     */
+    public EmptyContainer(String id, IModel<?> model) {
+        super(id, model);
+    }
+
+    /**
+     * Returns {@code true} if the model object of this component is empty.
+     */
+    public boolean isModelEmpty() {
+        return empty(getDefaultModelObject());
+    }
+
+    /**
+     * Sets the visibility of this component to {@code true} if
+     * {@link #isModelEmpty} returns {@code true}. Otherwise sets visibility to
+     * {@code false}.
+     */
+    @Override
+    protected void onConfigure() {
+        super.onConfigure();
+        setVisible(isModelEmpty());
+    }
+
+    private static boolean empty(Object obj) {
+        boolean empty = false;
+        if (null == obj) {
+            empty = true;
+        } else if (obj instanceof Boolean) {
+            empty = !(Boolean) obj;
+        } else if (obj instanceof Collection) {
+            empty = ((Collection<?>) obj).size() == 0;
+        } else if (obj instanceof String) {
+            empty = Strings.isEmpty((String) obj);
+        } else if (obj.getClass().isArray()) {
+            empty = ((Object[]) obj).length == 0;
+        }
+        return empty;
+    }
+
+}
